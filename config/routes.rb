5fedd4df45d825df264bd0d12633255bac24a2a0
Kat Brandt <joyfulkat11@gmail.com>
2022-07-11 16:51:33 -0600

    Fixes link from task index to task new.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   # get '/tasks', to: 'tasks#index'
-  # get '/tasks/:id', to: 'tasks#show'
   # get '/tasks/new', to: 'tasks#new'
+  # get '/tasks/:id', to: 'tasks#show'
   # post '/tasks', to: 'tasks#create'
   # get '/tasks/:id/edit', to: 'tasks#edit'
   # patch '/tasks/:id', to: 'tasks#update'
