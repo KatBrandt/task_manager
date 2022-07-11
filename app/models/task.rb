986d572cac7dace9c453b1e509784e5320c2b8ec
Kat Brandt <joyfulkat11@gmail.com>
2022-07-11 17:20:51 -0600

    Adds test and method for laundry.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
-
+  def laundry?
+    if title == 'laundry'
+      true
+    else
+      false
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+describe Task, type: :model do
+  #tells RSpec we are testing the Task class
+  # type: :model tells it this is a model (could leave out
+  # since this is located inside the models dir)
+  describe 'instance methods' do
+    describe '#laundry?' do
+      it 'returns true when the title is laundry' do
+        task = Task.create!(title: 'laundry', description: 'wash and fold baby')
 
+        expect(task.laundry?). to be true
+      end
+    end
+  end
 end
