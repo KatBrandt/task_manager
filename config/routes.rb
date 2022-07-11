7564979f5a28d34aeaf3df7c25b4fa0a80292da9
Kat Brandt <joyfulkat11@gmail.com>
2022-07-10 22:32:10 -0600

    Adds notes for myself.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
 
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
-  get '/tasks/new', to: 'tasks#new'
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id/edit', to: 'tasks#edit'
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/:id', to: 'tasks#show'
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
+  #The above can all refactor to:
+  resources :tasks
 end
