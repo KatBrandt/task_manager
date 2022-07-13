527d11f81d4f26a84d630bd80f4f10e7c98c5590
Kat Brandt <joyfulkat11@gmail.com>
2022-07-13 15:23:29 -0600

    Adds feature test for creating a new task.

---
+++ spec/features/tasks/new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'New Task', type: :feature do
+  describe 'As a visitor' do
+    describe 'When I click on New Task from the index page' do
+      it 'can create a new task' do
+        visit '/tasks'
+
+        click_link 'New Task'
+
+        expect(current_path).to eq '/tasks/new'
+
+        fill_in 'Title', with: 'Dishes'
+        fill_in 'Description', with: 'Wash them'
+        click_on 'Create Task'
+
+        expect(current_path).to eq '/tasks'
+        expect(page).to have_content 'Dishes'
+      end
+    end
+  end
+end
